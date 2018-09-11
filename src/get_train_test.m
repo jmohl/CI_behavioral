6f73e167c19f0b6ca9839b0e65243b7b15a42eca
jmohl <mohlymohly@gmail.com>
2018-09-11 18:09:33 -0400

    k fold
    just added k fold sorting for validation

--- src/get_train_test.m
+++ src/get_train_test.m
@@ -9,35 +9,20 @@
 % into train and test sets, and only comparing the likelihood on test sets.
 %
 % Methods
-% halved - equally splits each condition type in half, and return the
-% resulting data structures as 2 1x20 cell arrays, matching the input
-% structure
-% k-fold - uses k-fold cross validation where the train set conists of
+
+% kfold - uses k-fold cross validation where the train set conists of
 % (k-1)/k fraction of the data and the test set is the remaining 1/k
-% fraction of triaks. k is specified in options.kfolds and the data is
-% returned as 2 kx20 cell arrays.
+% fraction of trials. Trials are selected evenly but not randomly
+% throughout dataset. k is specified in options.kfolds and the data is
+% returned as an array where each row is a different fold.
 
 
 %% split data into train and test sets
-function [AV_train, AV_test]=get_train_test(AV_data,method,options)
 
-switch method
-    case 'halved'
-        for i = 1:length(AV_data)
-            train_ind = logical(zeros(length(AV_data{i}),1));
-            %randomly pick half the trials in each condition to assign to training
-            %dataset, setting those rows to 1
-            ntrs = length(AV_data{i});
-            train_ind(datasample(1:ntrs,round(ntrs/2),'Replace',false)) = 1;
-
-            AV_train{i} = AV_data{i}(train_ind);
-            AV_test{i} = AV_data{i}(~train_ind);
+function [train_inds, test_inds]=get_train_test(data,k)
+    test_inds = zeros(k,height(data));
+        for i = 1:k
+            test_inds(i,i:k:height(data)) = 1;
         end
-    case 'k-fold'
-        k=options.kfolds;
-        
-        train_ind = logical(zeros(length(AV_data{i}),k));
-        
-    otherwise
-        disp('method not recognized')
+        train_inds = ~test_inds;
 end
